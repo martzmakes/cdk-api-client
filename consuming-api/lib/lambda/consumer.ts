f2176189671d1cd5caf743c0b0f273fefdf44248
Matt Martz <matt.martz@gmail.com>
2025-03-29 10:26:18 -0400

    example client usage

---
+++ consuming-api/lib/lambda/consumer.ts
@@ -0,0 +1,10 @@
+import { createPublishedApiApiClient } from "@martzmakes/published-api";
+
+export const handler = async () => {
+  const apiClient = createPublishedApiApiClient();
+
+  const topping = await apiClient.getToppingByName({
+    toppingName: "Pepperoni",
+  });
+  topping.
+};
