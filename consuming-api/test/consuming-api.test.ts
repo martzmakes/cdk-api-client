5d0f8d904e7d2727000f525937b3466092901f39
Matt Martz <matt.martz@gmail.com>
2025-03-29 08:40:59 -0400

    first commit

---
+++ consuming-api/test/consuming-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ConsumingApi from '../lib/consuming-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/consuming-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ConsumingApi.ConsumingApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
